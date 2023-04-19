3db27735aee15d707533834acb1e66ac880e91e6
Oswaldo TC <40254158+dev-oswld@users.noreply.github.com>
2023-04-19 21:38:40 +0000

    Simple setup

--- PlaywrightDemo/PlaywrightBasics.cs
+++ PlaywrightDemo/PlaywrightBasics.cs
@@ -1,167 +1,167 @@
-namespace PlaywrightDemo;
+// namespace PlaywrightDemo;
 
-public class Tests
-{
-    [SetUp]
-    public void Setup()
-    {
-    }
+// public class Tests
+// {
+//     [SetUp]
+//     public void Setup()
+//     {
+//     }
 
-    [Test]
-    public async Task Test1()
-    {
-        //Playwright
-        using var playwright = await Playwright.CreateAsync();
-        //Browser
-        await using var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
-        {
-            Headless = false
-        });
-        //Page
-        var page = await browser.NewPageAsync();
-        await page.GotoAsync("http://www.eaapp.somee.com");
-        await page.ClickAsync("text=Login");
-        await page.ScreenshotAsync(new PageScreenshotOptions
-        {
-            Path = "EAApp.jpg"
-        });
-        await page.FillAsync("#UserName", "admin");
-        await page.FillAsync("#Password", "password");
-        await page.ClickAsync("text=Log in");
-        var isExist = await page.Locator("text='Employee Details'").IsVisibleAsync();
-        Assert.IsTrue(isExist);
-    }
+//     [Test]
+//     public async Task Test1()
+//     {
+//         //Playwright
+//         using var playwright = await Playwright.CreateAsync();
+//         //Browser
+//         await using var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
+//         {
+//             Headless = false
+//         });
+//         //Page
+//         var page = await browser.NewPageAsync();
+//         await page.GotoAsync("http://www.eaapp.somee.com");
+//         await page.ClickAsync("text=Login");
+//         await page.ScreenshotAsync(new PageScreenshotOptions
+//         {
+//             Path = "EAApp.jpg"
+//         });
+//         await page.FillAsync("#UserName", "admin");
+//         await page.FillAsync("#Password", "password");
+//         await page.ClickAsync("text=Log in");
+//         var isExist = await page.Locator("text='Employee Details'").IsVisibleAsync();
+//         Assert.IsTrue(isExist);
+//     }
 
-    [Test]
-    public async Task TestWithPOM()
-    {
-        //Playwright
-        using var playwright = await Playwright.CreateAsync();
-        //Browser
-        await using var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
-        {
-            Headless = false
-        });
-        //Page
-        var page = await browser.NewPageAsync();
-        await page.GotoAsync("http://www.eaapp.somee.com");
+//     [Test]
+//     public async Task TestWithPOM()
+//     {
+//         //Playwright
+//         using var playwright = await Playwright.CreateAsync();
+//         //Browser
+//         await using var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
+//         {
+//             Headless = false
+//         });
+//         //Page
+//         var page = await browser.NewPageAsync();
+//         await page.GotoAsync("http://www.eaapp.somee.com");
 
-        var loginPage = new LoginPageUpgraded(page);
-        await loginPage.ClickLogin();
-        await loginPage.Login("admin", "password");
-        var isExist = await loginPage.IsEmployeeDetailsExists();
-        Assert.IsTrue(isExist);
-    }
+//         var loginPage = new LoginPageUpgraded(page);
+//         await loginPage.ClickLogin();
+//         await loginPage.Login("admin", "password");
+//         var isExist = await loginPage.IsEmployeeDetailsExists();
+//         Assert.IsTrue(isExist);
+//     }
 
 
-    [Test]
-    public async Task WaitTest()
-    {
-        using var playwright = await Playwright.CreateAsync();
-        await using var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
-        {
-            Headless = false
-        });
-        var context = await browser.NewContextAsync();
-        var page = await context.NewPageAsync();
-        await page.GotoAsync("https://demos.telerik.com/kendo-ui/window/angular");
-        await page.Locator(
-                "text=Calendar May 2022SuMoTuWeThFrSa1234567891011121314151617181920212223242526272829 >> [aria-label=\"Close\"]")
-            .ClickAsync();
-        await page.Locator(
-                "text=Calendar May 2022SuMoTuWeThFrSa1234567891011121314151617181920212223242526272829 >> [aria-label=\"Close\"]")
-            .ClickAsync();
-        // Click button:has-text("Open AJAX content")
-        await page.Locator("button:has-text(\"Open AJAX content\")").ClickAsync();
-    }
+//     [Test]
+//     public async Task WaitTest()
+//     {
+//         using var playwright = await Playwright.CreateAsync();
+//         await using var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
+//         {
+//             Headless = false
+//         });
+//         var context = await browser.NewContextAsync();
+//         var page = await context.NewPageAsync();
+//         await page.GotoAsync("https://demos.telerik.com/kendo-ui/window/angular");
+//         await page.Locator(
+//                 "text=Calendar May 2022SuMoTuWeThFrSa1234567891011121314151617181920212223242526272829 >> [aria-label=\"Close\"]")
+//             .ClickAsync();
+//         await page.Locator(
+//                 "text=Calendar May 2022SuMoTuWeThFrSa1234567891011121314151617181920212223242526272829 >> [aria-label=\"Close\"]")
+//             .ClickAsync();
+//         // Click button:has-text("Open AJAX content")
+//         await page.Locator("button:has-text(\"Open AJAX content\")").ClickAsync();
+//     }
 
-    [Test]
-    public async Task TestNetwork()
-    {
-        //Playwright
-        using var playwright = await Playwright.CreateAsync();
-        //Browser
-        await using var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
-        {
-            Headless = false
-        });
-        //Page
-        var page = await browser.NewPageAsync();
-        await page.GotoAsync("http://www.eaapp.somee.com");
+//     [Test]
+//     public async Task TestNetwork()
+//     {
+//         //Playwright
+//         using var playwright = await Playwright.CreateAsync();
+//         //Browser
+//         await using var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
+//         {
+//             Headless = true
+//         });
+//         //Page
+//         var page = await browser.NewPageAsync();
+//         await page.GotoAsync("https://demo.applitools.com/");
 
-        var loginPage = new LoginPageUpgraded(page);
-        await loginPage.ClickLogin();
-        await loginPage.Login("admin", "password");
+//         var loginPage = new LoginPageUpgraded(page);
+//         await loginPage.ClickLogin();
+//         await loginPage.Login("admin", "password");
 
-        //Wait for response - Way 1
-        var waitResponse = page.WaitForResponseAsync("**/Employee");
-        //while the button is clicked
-        await loginPage.ClickEmployeeList();
-        //give the response details
-        var getResponse = await waitResponse;
+//         //Wait for response - Way 1
+//         var waitResponse = page.WaitForResponseAsync("**/Employee");
+//         //while the button is clicked
+//         await loginPage.ClickEmployeeList();
+//         //give the response details
+//         var getResponse = await waitResponse;
 
-        //Way 2 - Wait for response
-        var response = await page.RunAndWaitForResponseAsync(async () => { await loginPage.ClickEmployeeList(); },
-            x => x.Url.Contains("/Employee") && x.Status == 200);
+//         //Way 2 - Wait for response
+//         var response = await page.RunAndWaitForResponseAsync(async () => { await loginPage.ClickEmployeeList(); },
+//             x => x.Url.Contains("/Employee") && x.Status == 200);
 
-        var isExist = await loginPage.IsEmployeeDetailsExists();
-        Assert.IsTrue(isExist);
-    }
+//         var isExist = await loginPage.IsEmployeeDetailsExists();
+//         Assert.IsTrue(isExist);
+//     }
 
-    [Test]
-    public async Task Flipkart()
-    {
-        using var playwright = await Playwright.CreateAsync();
-        await using var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
-        {
-            Headless = false
-        });
-        var context = await browser.NewContextAsync();
-        var page = await context.NewPageAsync();
-        await page.GotoAsync("https://www.flipkart.com/", new PageGotoOptions
-        {
-            WaitUntil = WaitUntilState.NetworkIdle
-        });
-        await page.Locator("text=✕").ClickAsync();
+//     [Test]
+//     public async Task Flipkart()
+//     {
+//         using var playwright = await Playwright.CreateAsync();
+//         await using var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
+//         {
+//             Headless = true
+//         });
+//         var context = await browser.NewContextAsync();
+//         var page = await context.NewPageAsync();
+//         await page.GotoAsync("https://demo.applitools.com/, new PageGotoOptions
+//         {
+//             WaitUntil = WaitUntilState.NetworkIdle
+//         });
+//         await page.Locator("text=✕").ClickAsync();
 
-        await page.Locator("a", new PageLocatorOptions
-        {
-            HasTextString = "Login"
-        }).ClickAsync();
+//         await page.Locator("a", new PageLocatorOptions
+//         {
+//             HasTextString = "Login"
+//         }).ClickAsync();
 
-        var request = await page.RunAndWaitForRequestAsync(async () => { await page.Locator("text=✕").ClickAsync(); },
-            x => x.Url.Contains("flipkart.d1.sc.omtrdc.net") && x.Method == "GET");
+//         var request = await page.RunAndWaitForRequestAsync(async () => { await page.Locator("text=✕").ClickAsync(); },
+//             x => x.Url.Contains("flipkart.d1.sc.omtrdc.net") && x.Method == "GET");
 
-        var returnData = HttpUtility.UrlDecode(request.Url);
+//         var returnData = HttpUtility.UrlDecode(request.Url);
 
-        returnData.Should().Contain("Account Login:Displayed Exit");
-    }
+//         returnData.Should().Contain("Account Login:Displayed Exit");
+//     }
 
-    [Test]
-    public async Task? FlipkartNetworkInterception()
-    {
-        using var playwright = await Playwright.CreateAsync();
-        await using var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
-        {
-            Headless = false
-        });
-        var context = await browser.NewContextAsync();
-        var page = await context.NewPageAsync();
+//     [Test]
+//     public async Task? FlipkartNetworkInterception()
+//     {
+//         using var playwright = await Playwright.CreateAsync();
+//         await using var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
+//         {
+//             Headless = false
+//         });
+//         var context = await browser.NewContextAsync();
+//         var page = await context.NewPageAsync();
 
-        page.Request += (_, request) => Console.WriteLine(request.Method + "---" + request.Url);
-        page.Response += (_, response) => Console.WriteLine(response.Status + "---" + response.Url);
+//         page.Request += (_, request) => Console.WriteLine(request.Method + "---" + request.Url);
+//         page.Response += (_, response) => Console.WriteLine(response.Status + "---" + response.Url);
 
-        await page.RouteAsync("**/*", async route =>
-        {
-            if (route.Request.ResourceType == "image")
-                await route.AbortAsync();
-            else
-                await route.ContinueAsync();
-        });
+//         await page.RouteAsync("**/*", async route =>
+//         {
+//             if (route.Request.ResourceType == "image")
+//                 await route.AbortAsync();
+//             else
+//                 await route.ContinueAsync();
+//         });
 
-        await page.GotoAsync("https://www.flipkart.com/", new PageGotoOptions
-        {
-            WaitUntil = WaitUntilState.NetworkIdle
-        });
-    }
-}
+//         await page.GotoAsync("https://www.flipkart.com/", new PageGotoOptions
+//         {
+//             WaitUntil = WaitUntilState.NetworkIdle
+//         });
+//     }
+// }
--- PlaywrightDemo/PlaywrightLocator.cs
+++ PlaywrightDemo/PlaywrightLocator.cs
@@ -8,59 +8,59 @@ public class PlaywrightLocator
         using var playwright = await Playwright.CreateAsync();
         await using var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
         {
-            Headless = false,
+            Headless = true,
         });
         var context = await browser.NewContextAsync();
 
         var page = await context.NewPageAsync();
 
-        await page.GotoAsync("http://eaapp.somee.com/");
+        await page.GotoAsync("https://demo.applitools.com/");
 
-        await page.GetByRole(AriaRole.Link, new() { Name = "Login" }).ClickAsync();
+        // await page.GetByRole(AriaRole.Link, new() { Name = "Login" }).ClickAsync();
 
-        await page.GetByLabel("UserName").ClickAsync();
+        // await page.GetByLabel("UserName").ClickAsync();
 
-        await page.GetByLabel("UserName").FillAsync("admin");
+        // await page.GetByLabel("UserName").FillAsync("admin");
 
-        await page.GetByLabel("UserName").PressAsync("Tab");
+        // await page.GetByLabel("UserName").PressAsync("Tab");
 
-        await page.GetByLabel("Password").FillAsync("password");
+        // await page.GetByLabel("Password").FillAsync("password");
 
-        await page.GetByRole(AriaRole.Button, new() { Name = "Log in" }).ClickAsync();
+        // await page.GetByRole(AriaRole.Button, new() { Name = "Log in" }).ClickAsync();
 
-        await page.GetByRole(AriaRole.Link, new() { Name = "Employee List" }).ClickAsync();
+        // await page.GetByRole(AriaRole.Link, new() { Name = "Employee List" }).ClickAsync();
 
-        await page.GetByRole(AriaRole.Link, new() { Name = "Create New" }).ClickAsync();
+        // await page.GetByRole(AriaRole.Link, new() { Name = "Create New" }).ClickAsync();
 
-        await page.GetByLabel("Name").ClickAsync();
+        // await page.GetByLabel("Name").ClickAsync();
 
-        await page.GetByLabel("Name").FillAsync("Adam");
+        // await page.GetByLabel("Name").FillAsync("Adam");
 
-        await page.GetByLabel("Name").PressAsync("Tab");
+        // await page.GetByLabel("Name").PressAsync("Tab");
 
-        await page.GetByLabel("Salary").FillAsync("10000");
+        // await page.GetByLabel("Salary").FillAsync("10000");
 
-        await page.GetByLabel("Salary").PressAsync("Tab");
+        // await page.GetByLabel("Salary").PressAsync("Tab");
 
-        await page.GetByLabel("DurationWorked").FillAsync("1");
+        // await page.GetByLabel("DurationWorked").FillAsync("1");
 
-        await page.GetByLabel("DurationWorked").PressAsync("Tab");
+        // await page.GetByLabel("DurationWorked").PressAsync("Tab");
 
-        await page.GetByLabel("Grade").FillAsync("1");
+        // await page.GetByLabel("Grade").FillAsync("1");
 
-        await page.GetByLabel("Grade").PressAsync("Tab");
+        // await page.GetByLabel("Grade").PressAsync("Tab");
 
-        await page.GetByLabel("Email").FillAsync("adam@adam.com");
+        // await page.GetByLabel("Email").FillAsync("adam@adam.com");
 
-        await page.GetByRole(AriaRole.Button, new() { Name = "Create" }).ClickAsync();
+        // await page.GetByRole(AriaRole.Button, new() { Name = "Create" }).ClickAsync();
 
-        await page
-            .GetByRole(AriaRole.Row, new() { Name = "Adam" })
-            .GetByRole(AriaRole.Link, new() { Name = "Delete" }).ClickAsync();
+        // await page
+        //     .GetByRole(AriaRole.Row, new() { Name = "Adam" })
+        //     .GetByRole(AriaRole.Link, new() { Name = "Delete" }).ClickAsync();
 
         await page.ScreenshotAsync(new()
         {
-            Path = "screenshot.png",
+            Path = "./Outcome/screenshot.png",
             FullPage = true
         });
     }
@@ -71,86 +71,90 @@ public class PlaywrightLocator
  using var playwright = await Playwright.CreateAsync();
         await using var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
         {
-            Headless = false,
+            Headless = true,
         });
         var context = await browser.NewContextAsync();
 
         var page = await context.NewPageAsync();
 
-        await page.GotoAsync("https://executeautomation.com/");
+        await page.GotoAsync("https://demo.applitools.com/");
 
-        await page.GetByRole(AriaRole.Link, new() { Name = "Sign In" }).ClickAsync();
+        Assert.AreEqual(5,4);
 
-        await page.GetByRole(AriaRole.Link, new() { Name = "Sign Up" }).ClickAsync();
+        // await page.GetByRole(AriaRole.Link, new() { Name = "Sign In" }).ClickAsync();
+
+        // await page.GetByRole(AriaRole.Link, new() { Name = "Sign Up" }).ClickAsync();
         
-        await page.GetByPlaceholder("First Name").FillAsync("DemoUser1");
+        // await page.GetByPlaceholder("First Name").FillAsync("DemoUser1");
 
-        await page.GetByPlaceholder("Last Name").FillAsync("KK");
+        // await page.GetByPlaceholder("Last Name").FillAsync("KK");
 
-        await page.Locator("#react-select-2-input").FillAsync("New");
+        // await page.Locator("#react-select-2-input").FillAsync("New");
 
-        await page.GetByText("New Zealand", new() { Exact = true }).ClickAsync();
+        // await page.GetByText("New Zealand", new() { Exact = true }).ClickAsync();
 
-        await page.GetByPlaceholder("Email Address").FillAsync("demouser1@demo1.com");
+        // await page.GetByPlaceholder("Email Address").FillAsync("demouser1@demo1.com");
 
-        await page.GetByPlaceholder("Password", new() { Exact = true }).FillAsync("password");
+        // await page.GetByPlaceholder("Password", new() { Exact = true }).FillAsync("password");
 
-        await page.GetByPlaceholder("Enter Password Again").FillAsync("password");
+        // await page.GetByPlaceholder("Enter Password Again").FillAsync("password");
 
-        await page.GetByPlaceholder("Profession").FillAsync("Tester");
+        // await page.GetByPlaceholder("Profession").FillAsync("Tester");
 
-        await page.GetByRole(AriaRole.Button, new() { Name = "Create Account" }).ClickAsync();
+        // await page.GetByRole(AriaRole.Button, new() { Name = "Create Account" }).ClickAsync();
 
-        await page.Locator("img:nth-child(2)").ClickAsync();
+        // await page.Locator("img:nth-child(2)").ClickAsync();
 
-        await page.GetByRole(AriaRole.Button, new() { Name = "Create Account" }).ClickAsync();
+        // await page.GetByRole(AriaRole.Button, new() { Name = "Create Account" }).ClickAsync();
 
-        await page.Locator(".ct-logo-header").ClickAsync();
+        // await page.Locator(".ct-logo-header").ClickAsync();
 
-        await page.GetByRole(AriaRole.Link, new() { Name = "Codeless automation Lean" }).ClickAsync();
+        // await page.GetByRole(AriaRole.Link, new() { Name = "Codeless automation Lean" }).ClickAsync();
 
-        await page.GetByRole(AriaRole.Link, new() { Name = "1 Katalon Studio" }).ClickAsync();
+        // await page.GetByRole(AriaRole.Link, new() { Name = "1 Katalon Studio" }).ClickAsync();
 
-        await page.GetByRole(AriaRole.Button, new() { Name = "Watch" }).ClickAsync();
+        // await page.GetByRole(AriaRole.Button, new() { Name = "Watch" }).ClickAsync();
 
-        await page.GetByRole(AriaRole.Heading, new() { Name = "Part 2 - Record & playback," }).ClickAsync();
+        // await page.GetByRole(AriaRole.Heading, new() { Name = "Part 2 - Record & playback," }).ClickAsync();
 
-        await page.GetByRole(AriaRole.Button, new() { Name = "Close" }).ClickAsync();
+        // await page.GetByRole(AriaRole.Button, new() { Name = "Close" }).ClickAsync();
     }
 
 
     [Test]
-    public async Task LocatorTestsForEA2()
+    public async Task TestCaseHeadLTurnOn()
     {
         using var playwright = await Playwright.CreateAsync();
         await using var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
         {
-            Headless = false,
+            Headless = true,
         });
         var context = await browser.NewContextAsync();
 
         var page = await context.NewPageAsync();
 
-        await page.GotoAsync("http://localhost:8000/Product/Create");
+        await page.GotoAsync("https://demo.applitools.com/");
 
-        var form1 = page.GetByRole(AriaRole.Form, new() { Name = "form1" });
-        var form2 = page.GetByRole(AriaRole.Form, new() { Name = "form2" });
+        Console.WriteLine("keyboard");
 
-        await form1.GetByLabel("Name").FillAsync("keyboard");
+        // var form1 = page.GetByRole(AriaRole.Form, new() { Name = "form1" });
+        // var form2 = page.GetByRole(AriaRole.Form, new() { Name = "form2" });
 
-        await form1.GetByLabel("Description").FillAsync("gaming keyboard");
+        // await form1.GetByLabel("Name").FillAsync("keyboard");
 
-        await form1.GetByRole(AriaRole.Spinbutton).FillAsync("20");
+        // await form1.GetByLabel("Description").FillAsync("gaming keyboard");
 
-        await form1.Locator("#ProductType").SelectOptionAsync(new[] { "1" });
+        // await form1.GetByRole(AriaRole.Spinbutton).FillAsync("20");
 
-        await form2.Locator("#Name").FillAsync("Mouse");
+        // await form1.Locator("#ProductType").SelectOptionAsync(new[] { "1" });
 
-        await form2.Locator("#Description").FillAsync("Gaming mouse");
+        // await form2.Locator("#Name").FillAsync("Mouse");
 
-        await form2.GetByRole(AriaRole.Spinbutton).FillAsync("10");
+        // await form2.Locator("#Description").FillAsync("Gaming mouse");
 
-        await form2.Locator("#ProductType").SelectOptionAsync(new[] { "1" });
+        // await form2.GetByRole(AriaRole.Spinbutton).FillAsync("10");
+
+        // await form2.Locator("#ProductType").SelectOptionAsync(new[] { "1" });
 
     }
 }
